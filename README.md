ab0b0c73cde4a967449e3698c7be3c3698d7b187
GOTO Satoru <satoru.goto@appdynamics.com>
2020-07-06 10:51:21 +0900

    for Node.js 10

---
+++ README.md
@@ -0,0 +1,33 @@
+# Google Cloud Functions - stop GCE Instances sample
+
+forked from https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
+
+## reference document: https://cloud.google.com/scheduler/docs/start-and-stop-compute-engine-instances-on-a-schedule
+
+## create Pub/Sub topics
+
+```
+gcloud pubsub topics create stop-instance-event
+gcloud pubsub topics list
+```
+
+## deploy function
+
+Do not use Node.js 8 run time
+
+```
+git clone https://github.com/goto-satoru/cloud-functions-sample-nodejs10.git
+cd cloud-functions-sample-nodejs10/stop-instance
+gcloud functions deploy stopInstancePubSub --trigger-topic stop-instance-event --runtime nodejs10 --region=asia-northeast1
+gcloud functions list
+```
+
+## create scheduler jobs
+
+stop all GCE instance in asia-northeast1-c and asia-northeast1-b regions which have env=test label.
+
+```
+gcloud beta scheduler jobs create pubsub stop-instances --schedule '0 23 * * *' --topic stop-instance-event --message-body '{"zone":"asia-northeast1-c", "label":"env=test"}' --time-zone 'Asia/Tokyo'
+gcloud beta scheduler jobs create pubsub stop-instances --schedule '0 23 * * *' --topic stop-instance-event --message-body '{"zone":"asia-northeast1-b", "label":"env=test"}' --time-zone 'Asia/Tokyo'
+gcloud beta scheduler jobs list
+```
---
+++ stop-instance/index.js
@@ -0,0 +1,46 @@
+const Compute = require('@google-cloud/compute');
+const compute = new Compute();
+
+exports.stopInstancePubSub = async (event, context, callback) => {
+  try {
+    const payload = _validatePayload(
+      JSON.parse(Buffer.from(event.data, 'base64').toString())
+    );
+    const options = {filter: `labels.${payload.label}`};
+    const [vms] = await compute.getVMs(options);
+    await Promise.all(
+      vms.map(async (instance) => {
+        console.log(instance);
+        if (payload.zone === instance.zone.id) {
+          console.log(instance);
+          const [operation] = await compute
+            .zone(payload.zone)
+            .vm(instance.name)
+            .stop();
+
+          // Operation pending
+          return operation.promise();
+        } else {
+          return Promise.resolve();
+        }
+      })
+    );
+
+    // Operation complete. Instance successfully stopped.
+    const message = `Successfully stopped instance(s)`;
+    console.log(message);
+    callback(null, message);
+  } catch (err) {
+    console.log(err);
+    callback(err);
+  }
+};
+
+const _validatePayload = (payload) => {
+  if (!payload.zone) {
+    throw new Error(`Attribute 'zone' missing from payload`);
+  } else if (!payload.label) {
+    throw new Error(`Attribute 'label' missing from payload`);
+  }
+  return payload;
+};
